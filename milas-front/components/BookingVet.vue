996a051b90d647c180af8b7fb0af6964923a8446
christian-rojas <christian.ici17@gmail.com>
2023-04-25 16:50:16 -0400

    add datepicker

--- milas-front/components/BookingVet.vue
+++ milas-front/components/BookingVet.vue
@@ -1,13 +1,25 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <date-picker v-model="date" :config="options"></date-picker>
+  </div>
 </template>
 <script lang="ts">
+import datePicker from 'vue-bootstrap-datetimepicker'
 export default {
-    
+  components: { datePicker },
+  data() {
+    return {
+      date: new Date(),
+      options: {
+        format: 'DD/MM/YYYY hh:mm',
+        useCurrent: false,
+        allowInputToggle: true,
+      },
+    }
+  },
+  created() {
+    // eslint-disable-next-line no-new
+  },
 }
 </script>
-<style lang="css">
-    
-</style>
+<style lang="css"></style>
--- milas-front/pages/index.vue
+++ milas-front/pages/index.vue
@@ -3,16 +3,19 @@
     <NavBar />
     <div class="container">
       <carrousel-home />
+      <hr>
+      <booking-vet></booking-vet>
     </div>
   </div>
 </template>
 
 <script>
 import CarrouselHome from '../components/Carrousel-home.vue';
+import BookingVet from '../components/BookingVet.vue';
 import NavBar from '~/components/NavBar.vue';
 
 export default {
     name: "IndexPage",
-    components: { NavBar, CarrouselHome }
+    components: { NavBar, CarrouselHome, BookingVet }
 }
 </script>
